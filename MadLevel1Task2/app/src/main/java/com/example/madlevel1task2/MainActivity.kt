7cbd6a97f334273c5faf39c7de90144a5cb979b8
omgitsarnout <arnout@keuzestress.nl>
2020-09-20 02:17:19 +0200

    Level1-task2 finished

---
+++ MadLevel1Task2/app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.madlevel1task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSubmit.setOnClickListener {
+            checkAnswers()
+        }
+    }
+
+    private fun checkAnswers() {
+        if (txt1.getText().toString().equals("T") && txt2.getText().toString().equals("F") && txt3.getText().toString().equals("F") && txt4.getText().toString().equals("F")) {
+            onAnswerCorrect()
+        } else {
+            onAnswerIncorrect()
+        }
+    }
+
+    /**
+     * Displays a successful Toast message.
+     */
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * Displays an incorrect Toast message.
+     */
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+    }
+
+
+}
