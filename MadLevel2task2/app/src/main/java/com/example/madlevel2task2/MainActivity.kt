99775f7a8f7d0ddb516ebce34635155fde34c3d5
omgitsarnout <arnout@keuzestress.nl>
2020-09-22 00:14:12 +0200

    Level2-task2 first build

---
+++ MadLevel2task2/app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.madlevel2task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.madlevel2task2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+    // Don't forget to create a binding object as you did in previous assignments.
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        // Initialize the recycler view with a linear layout manager, adapter
+        binding.rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        questions.add(Question("A 'Val' and 'var' are the same.", false))
+        questions.add(Question("Mobile Application Development grants 12 ECTS.", false))
+        questions.add(Question("A Unit in Kotlin corresponds to a void in Java", true))
+        questions.add(Question("In Kotlin, 'when' replaces the 'switch' operator in Java", true))
+        
+        questionAdapter.notifyDataSetChanged()
+    }
+}
