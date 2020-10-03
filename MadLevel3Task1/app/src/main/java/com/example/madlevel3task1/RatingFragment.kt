f2906759225a30a500ec9e7f1256951fa5972832
omgitsarnout <arnout@keuzestress.nl>
2020-10-03 23:52:11 +0200

    MadLevel3Task1 finished

--- MadLevel3Task1/app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
+++ MadLevel3Task1/app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -24,8 +25,26 @@ class RatingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        btn_to_summary.setOnClickListener {
+            navigateToSummary()
         }
+
+        showRandomAssessableGame()
+    }
+
+    private fun navigateToSummary() {
+
+        val args = Bundle()
+        args.putFloat(ARG_GAME_RATING, rb_game.rating)
+        args.putString(ARG_GAME_NAME, txt_game.text.toString())
+
+        findNavController().navigate(R.id.action_ratingFragment_to_summaryFragment, args)
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf("Red Dead Redemption 2", "Rocket League",
+            "Shadow of the Tombraider").random()
+
+        txt_game.text = randomGame
     }
 }
