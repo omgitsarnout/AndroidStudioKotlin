fc31d8b49fd0eb6ce9ad05b4ca88314e63226b01
omgitsarnout <arnout@keuzestress.nl>
2020-11-06 17:08:03 +0100

    MadLevel5Example step1 finished

---
+++ MadLevel5Example/app/src/main/java/com/example/madlevel3example/ReminderDao.kt
@@ -0,0 +1,22 @@
+package com.example.madlevel3example
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.madlevel2example.Reminder
+
+@Dao
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminderTable")
+    fun getAllReminders(): LiveData<List<Reminder>>
+
+    @Insert
+    suspend fun insertReminder(reminder: Reminder)
+
+    @Delete
+    suspend fun deleteReminder(reminder: Reminder)
+
+    @Update
+    suspend fun updateReminder(reminder: Reminder)
+
+}
