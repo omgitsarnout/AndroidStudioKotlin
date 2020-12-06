1bdf0933633172891c4a23933d28415e4573cb35
omgitsarnout <arnout@keuzestress.nl>
2020-12-06 19:49:05 +0100

    MadLevel6Example finished

--- MadLevel6Example/app/src/main/java/com/example/madlevel6example/FirstFragment.kt
+++ MadLevel6Example/app/src/main/java/com/example/madlevel6example/FirstFragment.kt
@@ -6,12 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    private val viewModel: TriviaViewModel by activityViewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +30,23 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observeTrivia()
+
+
         /*view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }*/
     }
+
+    private fun observeTrivia() {
+        viewModel.trivia.observe(viewLifecycleOwner, Observer {
+            tvTrivia.text = it?.text
+        })
+
+        // Observe the error message.
+        viewModel.errorText.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+        })
+    }
+
 }
